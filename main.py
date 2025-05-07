eed62033cccc40f5547b5128b3c4b196417b2635
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2025-05-07 21:16:59 +0630

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow your Next.js frontend
+    allow_origins=["https://swelpaylar.vercel.app"],  # Allow your Next.js frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
